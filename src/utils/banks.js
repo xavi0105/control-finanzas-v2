8b7ace7bdf5cf5d33ede9f0e5af456d39bcc80b6
xavi0105 <javierpryser@gmail.com>
2026-08-19 17:01:30 -0600

    Bancos: agrega Banorte Nomina, Banamex Oro/Joy/Simplicity, BBVA Nomina/Azul/Oro/Platino, Link Card y Santander LikeU (Debito/Credito)

--- src/utils/banks.js
+++ src/utils/banks.js
@@ -1,10 +1,24 @@
 export const BANKS = [
   { code: 'none', name: 'Sin banco', short: '?', color: '#94a3b8' },
+
   { code: 'bbva', name: 'BBVA', short: 'BBVA', color: '#1973b8' },
+  { code: 'bbva-nomina', name: 'BBVA Nómina', short: 'NÓM', color: '#1973b8' },
+  { code: 'bbva-azul', name: 'BBVA Azul', short: 'AZUL', color: '#1973b8' },
+  { code: 'bbva-oro', name: 'BBVA Oro', short: 'ORO', color: '#1973b8' },
+  { code: 'bbva-platino', name: 'BBVA Platino', short: 'PLAT', color: '#1973b8' },
+
   { code: 'banamex', name: 'Citibanamex', short: 'B', color: '#c8102e' },
+  { code: 'banamex-oro', name: 'Banamex Oro', short: 'ORO', color: '#c8102e' },
+  { code: 'banamex-joy', name: 'Banamex Joy', short: 'JOY', color: '#c8102e' },
+  { code: 'banamex-simplicity', name: 'Banamex Simplicity', short: 'SIMPLE', color: '#c8102e' },
+
   { code: 'santander', name: 'Santander', short: 'S', color: '#ec0000' },
-  { code: 'hsbc', name: 'HSBC', short: 'HSBC', color: '#db0011' },
+  { code: 'santander-likeu', name: 'Santander LikeU (Débito/Crédito)', short: 'LIKEU', color: '#ec0000' },
+
   { code: 'banorte', name: 'Banorte', short: 'N', color: '#002b5c' },
+  { code: 'banorte-nomina', name: 'Banorte Nómina', short: 'NÓM', color: '#002b5c' },
+
+  { code: 'hsbc', name: 'HSBC', short: 'HSBC', color: '#db0011' },
   { code: 'scotiabank', name: 'Scotiabank', short: 'S', color: '#c41f3e' },
   { code: 'nu', name: 'Nu', short: 'Nu', color: '#820ad1' },
   { code: 'stori', name: 'Stori', short: 'S', color: '#00b3a4' },
@@ -16,7 +30,8 @@ export const BANKS = [
   { code: 'inbursa', name: 'Inbursa', short: 'I', color: '#00538c' },
   { code: 'amex', name: 'American Express', short: 'AMEX', color: '#2e77bc' },
   { code: 'visa', name: 'Visa', short: 'VISA', color: '#1a1f71' },
-  { code: 'mastercard', name: 'Mastercard', short: 'MC', color: '#eb001b' }
+  { code: 'mastercard', name: 'Mastercard', short: 'MC', color: '#eb001b' },
+  { code: 'link-card', name: 'Link Card (Invex)', short: 'LINK', color: '#111827' }
 ]
 
 export function bankByCode(code) {
